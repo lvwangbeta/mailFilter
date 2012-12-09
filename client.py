2e900d91eaa94515b6ab0e4cd68685b8c315a78e
gavin <lvwangbeta@gmail.com>
2012-12-09 17:36:15 +0800

    the client can choose the email location

--- client.py
+++ client.py
@@ -1,11 +1,17 @@
 # -*- coding: utf-8 -*-
 import socket
+import sys
 
 if __name__ == '__main__':
-    host = 'localhost'
-    port = 8888
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    msg = open(r"./email.txt").read()
-    s.connect((host, port))
-    s.sendall(msg)
-    s.close()
+	host = 'localhost'
+	port = 8888
+	try:
+		fi = str(sys.argv[1])
+		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		msg = open(fi).read()
+		s.connect((host, port))
+		s.sendall(msg)
+		s.close()		
+	except:
+		print "error: Input the email location"
+
